a0fdefa6288c352784f324c2190030b18a3fceee
awanzada786616 <wasicenter120@gmail.com>
2026-02-18 14:19:02 +0500

    Update order.js

--- api/order.js
+++ api/order.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
     const { service, link, quantity } = req.body;
-    const API_KEY = 'eca1d82bc97b56839285darknetac4f391e5fee7c66ec34';
+    const API_KEY = '47e3fd8ab22eb097fee4infosec1f4994d307570972e5d0';
     const API_URL = 'https://smmpakpanels.com/api/v2';
 
     try {
